47272bb12c38ad64a63f73eda805f69fab1e2f6a
iamAdrian <adriantnare@icloud.com>
2023-02-16 01:42:09 +0200

    fix: fix circular dependancy by using @Lazy .

--- src/main/java/com/sir_ad/myBlog_backend/config/SecurityConfiguration.java
+++ src/main/java/com/sir_ad/myBlog_backend/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import com.sir_ad.myBlog_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +24,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Lazy
     @Autowired
     private UserService userService;
 
